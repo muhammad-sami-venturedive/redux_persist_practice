64db6ec5a6347f8667d7c4e7c52d2df7faa41b60
Muhammad Sami <sami.siddiq@venturedive.com>
2023-06-23 11:06:36 +0500

    added current font size indiction in home screen

--- SRC/Screens/Home/Home.js
+++ SRC/Screens/Home/Home.js
@@ -1,17 +1,35 @@
 import React from 'react'
 import { StyleSheet, Text, View, Button } from 'react-native'
 import NavigationStrings from '../../Const/NavigationStrings/NavigationStrings'
+import { DECREASE_FONTSIZE, INCREASE_FONTSIZE } from '../../Redux/Action Types/ActionTypes'
+import { connect } from 'react-redux'
 
-const Home = ({ navigation }) => {
+const Home = ({ navigation, size }) => {
     return (
         <View style={STYLES.mainCont} >
-            <Text>Home Screen</Text>
+            <Text style={STYLES.text(size)} >Home Screen</Text>
+            <Text style={STYLES.text(size)} >Current Font Size: {size}</Text>
             <Button title='Goto Settings' onPress={() => navigation.navigate(NavigationStrings.SETTINGS)} />
         </View>
     )
 }
 
-export default Home
+const mapStateToProps = (state) => {
+    return {
+        size: state.FontSizeReducer.FontSize
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        increase: () => dispatch({ type: INCREASE_FONTSIZE }),
+        decrease: () => dispatch({ type: DECREASE_FONTSIZE }),
+    }
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home)
+
 
 const STYLES = StyleSheet.create({
     mainCont: {
@@ -20,4 +38,12 @@ const STYLES = StyleSheet.create({
         justifyContent: 'center',
         // flexDirection: 'row',
     },
+    subCont: {
+        justifyContent: 'space-around',
+        padding: 10,
+        margin: 10
+    },
+    text: (size) => ({
+        fontSize: size ? size : 18
+    })
 })
